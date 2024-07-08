c841718454b02c5a69aac39c65cfc0f478a0286a
sasidhar sunny <63310620+sasidharsunny@users.noreply.github.com>
2024-07-08 12:27:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,52 @@
-provider "aws" {
-    region = "ap-south-1"  
+#This Terraform Code Deploys Basic VPC Infra.
+
+
+
+
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.57.0"
+    }
+  }
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
-  instance_type = "t2.micro"
- tags = {
+
+////////////////AWS VPC///////////////////////////////////
+
+resource "aws_vpc" "example" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_subnet" "example" {
+  vpc_id            = aws_vpc.example.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+
+
+resource "aws_instance" "testing" {
+  ami           = "ami-0740aeaa53d2ea48a"
+  instance_type = "t4g.small"
+  subnet_id     = aws_subnet.example.id
+
+  cpu_options {
+    core_count       = 1
+    threads_per_core = 1
+  }
+
+  tags = {
     Name = "tf-example"
   }
 }
